8c95e384d36e7ed37fa650343b95b2591eccaa5d
lopez2730 <all_30@lvie.com>
2023-10-23 18:07:47 -0600

    1st commit

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>¿Quíen es este pokémon?</h1>
+    <PokemonPicture :pokemonId="600" :showPokemon="true"/>
+    <PokemonOptions />
+</template>
+
+
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture.vue'
+import PokemonOptions from '@/components/PokemonOptions.vue'
+
+import getPokemonOptions from '@/helpers/getPokemonOptions'
+
+console.log(getPokemonOptions())
+
+export default {
+    components: {
+        PokemonPicture,
+        PokemonOptions
+    }
+
+}
+</script>
